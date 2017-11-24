f5d10bbc0e7a90f6439400040dbeba2d81d20754
clementfung <clement.y.fung@gmail.com>
2017-11-23 18:50:53 -0800

    First version of dynamic dblp

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ NSS lab website, forked for easy dev
 
 Review pages with
 https://htmlpreview.github.io/?https://raw.githubusercontent.com/clementfung/www/master/html/index.html
+
+Dynamic dblp, use `wget http://dblp.org/search/publ/api?q=andrew_warfield -O andy.xml`, and replace with the faculty member you desire.
---
+++ dynamic-dblp/parser.py
@@ -0,0 +1,129 @@
+import xml.etree.ElementTree as ET
+from django.utils.encoding import smart_str
+
+import pdb
+
+keys_i_care_about = ['title', 'venue', 'year', 'ee']
+year_list = list()
+
+def parse_faculty_obj(pub_dict, faculty_file):
+
+    tree = ET.parse(faculty_file)
+    root = tree.getroot()
+    total_parsed = 0
+
+    # This whole part is very specific to how the DBLP API looks.
+    # The XML element "hits" is the one with everything
+    for hit in root.find('hits'):
+
+        # Get a unique id for each pub
+        pub_id = hit.get('id')
+        hit_info = hit[0]
+
+        # Are all needed tags there?
+        has_all = True
+
+        # A flat dictionary for one publication
+        pub_obj = dict()
+
+        if hit_info.find('year') is None:
+            continue
+
+        # Top level dictionary indexed by year.
+        year = hit_info.find('year').text
+        
+        # Skip anything outside of the desired year range
+        if year not in year_list:
+            continue
+
+        # Init year dict if not present
+        if year not in pub_dict:
+            pub_dict[year] = dict()
+
+        # Skip repearted publication ID
+        if pub_id in pub_dict[year]:
+            print pub_id + " is already present."
+            continue
+
+        # Get authors
+        if hit_info.find('authors') is None:
+            has_all = False
+
+        author_elems = hit_info.find('authors')
+        author_list = list()
+        for author in author_elems:
+            author_list.append(author.text)
+
+        pub_obj['authors'] = author_list
+
+        # Get all other keys
+        for key in keys_i_care_about:
+
+            if hit_info.find(key) is None:
+                has_all = False
+            else:
+                pub_obj[key] = hit_info.find(key).text
+
+        # Add to the faculty_obj if all keys are there
+        if has_all:
+
+            pub_dict[year][pub_id] = pub_obj
+            total_parsed += 1
+
+    return pub_dict, total_parsed
+
+
+def pub_dict_to_html(pub_dict):
+
+    file_obj = open("pub_list.html", 'w')
+
+    for year in year_list:
+
+        file_obj.write("<h3>" + year + "</h3>\n")
+        file_obj.write("<ul>\n")
+        year_dict = pub_dict[year]
+
+        for pub_id, pub in year_dict.items():
+
+            file_obj.write("<li>\n")
+            file_obj.write(
+                "<strong>" + smart_str(pub['title']) + "</strong>\n")
+
+            author_string = ", ".join(pub['authors'])
+
+            file_obj.write(smart_str(author_string) + ".\n")
+
+            file_obj.write(pub['venue'] + "\n")
+            file_obj.write(pub['year'] + ".\n")
+            file_obj.write(
+                "[<a href=\"" + smart_str(pub['ee']) + "\">link</a>]\n")
+            file_obj.write("</li>\n")
+
+        file_obj.write("</ul>\n")
+
+
+if __name__ == "__main__":
+
+    starting_year = 2017
+
+    # Take from the current year, go back 10 years
+    for i in range(15):
+        year_list.append(str(starting_year - i))
+
+    faculty_files = ["andy.xml", "ivan.xml", "bill.xml",
+                     'alan.xml', 'norm.xml', 'mike.xml']
+
+    # dictionary by year
+    pub_dict = dict()
+    total_count = 0
+
+    for file in faculty_files:
+        faculty_dict, count = parse_faculty_obj(pub_dict, file)
+        print "Parsed " + str(count) + " publications for " + file
+        total_count += count
+
+    print "Parsed " + str(total_count) + " in total."
+
+    pub_dict_to_html(pub_dict)
+
+    print "Done writing to HTML."
